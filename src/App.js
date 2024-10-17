53e61e26a3f6d354d1ee5d738cf1cdd9022ad4e5
Pranav <mpranavprem@gmail.com>
2024-10-17 14:09:19 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import Header from "./components/Header";
 import "react-modern-drawer/dist/index.css";
 import "keen-slider/keen-slider.min.css";
-// import { useContext } from "react";
 import { Toaster } from "react-hot-toast";
-// import { LoadingSpinner } from "./components/LoadingSpinner";
-// import { SpinnerContext } from "./components/SpinnerContext";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Footer from "./components/Footer";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useContext } from "react";
 import ContactFormStep1 from "./pages/ContactUs/ContactFormStep1";
 import ContactFormStep2 from "./pages/ContactUs/ContactFormStep2";
+import SpinnerContextProvider, {
+  SpinnerContext,
+} from "./components/SpinnerContext";
+import { LoadingSpinner } from "./components/LoadingSpinner";
+
 const LandingPage = lazy(() => import("./pages/LandingPage"));
 
 AOS.init({
@@ -22,50 +24,58 @@ AOS.init({
 });
 
 function App() {
-  // const { isLoading } = useContext(SpinnerContext);
   return (
-    <BrowserRouter>
-      {/* {isLoading && <LoadingSpinner />} */}
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          style: {
-            background: "#000000",
-            color: "#ffffff",
-          },
-        }}
-      />
-      <Suspense fallback={<div></div>}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <LandingPage />
-              <Footer />
-            </>
-          }
+    <SpinnerContextProvider>
+      <BrowserRouter>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            style: {
+              background: "#000000",
+              color: "#ffffff",
+            },
+          }}
         />
-        <Route
-          path="/contact/*"
-          element={
-            <>
-              <Header />
-              <Routes>
-                <Route index element={<Navigate to="step1" replace />} />
-                <Route path="step1" element={<ContactFormStep1 />} />
-                <Route path="step2" element={<ContactFormStep2 />} />
-              </Routes>
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
-      </Suspense>
-    </BrowserRouter>
+        <Suspense fallback={<LoadingSpinner />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <LandingPage />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/contact/*"
+              element={
+                <>
+                  <Header />
+                  <Routes>
+                    <Route index element={<Navigate to="step1" replace />} />
+                    <Route path="step1" element={<ContactFormStep1 />} />
+                    <Route path="step2" element={<ContactFormStep2 />} />
+                  </Routes>
+                  <Footer />
+                </>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      {/* Access context here after the provider wraps the application */}
+      <LoadingSpinnerContext />
+    </SpinnerContextProvider>
   );
 }
+
+// component to manage loading spinner based on context
+const LoadingSpinnerContext = () => {
+  const { spinner } = useContext(SpinnerContext);
+  return spinner && <LoadingSpinner />;
+};
 
 export default App;
