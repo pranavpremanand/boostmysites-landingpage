db6f4c17765304dbef7414f8da6207b4cf7738fc
Pranav <mpranavprem@gmail.com>
2024-10-18 15:51:11 +0530

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -70,104 +70,80 @@ const Header = () => {
       >
         <div className="w-full">
           <div className="wrapper flex justify-between items-center w-full">
-            {pathname.includes("contact") ? (
-              <RLink to="/" className="cursor-pointer">
-                <img
-                  src={logo}
-                  alt=""
-                  className="h-[4rem] md:h-[5rem] object-contain -mb-3"
-                />
-              </RLink>
-            ) : (
-              <Link
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={1000}
-                className="cursor-pointer"
-              >
-                <img
-                  src={logo}
-                  alt=""
-                  className="h-[4rem] md:h-[5rem] object-contain -mb-3"
-                />
-              </Link>
-            )}
-            {!pathname.includes("contact/step") && (
-              <div className="text-sm uppercase hidden lg:flex items-center gap-7">
-                {options.map((option) => (
-                  <Link
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={1000}
-                    to={option.path}
-                    key={option.id}
-                    className="text-secondary link cursor-pointer"
-                    activeClass="link-active"
-                  >
-                    {option.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-            {!pathname.includes("contact/step") && (
-              <RLink to="/contact" className="hidden lg:flex primary-btn1">
-                Let's Connect
-              </RLink>
-            )}
+            <RLink to="/home" className="cursor-pointer">
+              <img
+                src={logo}
+                alt=""
+                className="h-[4rem] md:h-[5rem] object-contain -mb-3"
+              />
+            </RLink>
+            <div className="text-sm uppercase hidden lg:flex items-center gap-7">
+              {options.map((option) => (
+                <Link
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1000}
+                  to={option.path}
+                  key={option.id}
+                  className="text-secondary link cursor-pointer"
+                  activeClass="link-active"
+                >
+                  {option.title}
+                </Link>
+              ))}
+            </div>
+            <RLink to="/contact" className="hidden lg:flex primary-btn1">
+              Let's Connect
+            </RLink>
 
-            {!pathname.includes("contact/step") && (
-              <div
-                className="block lg:hidden justify-self-end"
-                onClick={toggleDrawer}
-              >
-                <Hamburger
-                  color="#FFAB23"
-                  size="23"
-                  toggled={isOpen}
-                  rounded
-                  toggle={setIsOpen}
-                />
-              </div>
-            )}
+            <div
+              className="block lg:hidden justify-self-end"
+              onClick={toggleDrawer}
+            >
+              <Hamburger
+                color="#FFAB23"
+                size="23"
+                toggled={isOpen}
+                rounded
+                toggle={setIsOpen}
+              />
+            </div>
           </div>
         </div>
       </div>
-      {!pathname.includes("contact/step") && (
-        <Drawer
-          open={isOpen}
-          onClose={toggleDrawer}
-          direction="right"
-          className="py-4 px-10 z-10 bg-black text-secondary"
-        >
-          <div className="mb-6 flex items-center justify-end pr-[.7rem] py-[.4rem]">
-            <button
+
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="right"
+        className="py-4 px-10 z-10 bg-black text-secondary"
+      >
+        <div className="mb-6 flex items-center justify-end pr-[.7rem] py-[.4rem]">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="text-primary text-[2.2rem]"
+          >
+            <IoMdClose />
+          </button>
+        </div>
+        <div className="flex flex-col gap-4">
+          {options.map(({ title, path, id }) => (
+            <Link
               onClick={() => setIsOpen(false)}
-              className="text-primary text-[2.2rem]"
+              key={id}
+              className="text-2xl font-medium transition-colors duration-300 link"
+              to={path}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
             >
-              <IoMdClose />
-            </button>
-          </div>
-          <div className="flex flex-col gap-4">
-            {options.map(({ title, path, id }) => (
-              <Link
-                onClick={() => setIsOpen(false)}
-                key={id}
-                className="text-2xl font-medium transition-colors duration-300 link"
-                to={path}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={1000}
-              >
-                {title}
-              </Link>
-            ))}
-          </div>
-        </Drawer>
-      )}
+              {title}
+            </Link>
+          ))}
+        </div>
+      </Drawer>
     </>
   );
 };
